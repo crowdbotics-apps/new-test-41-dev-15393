5b26f861de72fea3c1d6b48ffebdd9a90079d944
Crowdbotics <team@crowdbotics.com>
2020-12-17 19:09:45 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,5 +1,10 @@
 import axios from "axios"
 import {} from "react-native-dotenv"
+const plugin = axios.create({
+  baseURL:
+    "https://crowdbotics-slack-dev.herokuapp.com/dashboard/app/7238/storyboard/8485/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 const weatherAPI = axios.create({
   baseURL: "https://www.weatherapi.com/",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
